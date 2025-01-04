4c43b3925cd3b0aa267c1f814c7ccf3e370b2edd
Empty <2167335204@qq.com>
2025-01-04 23:36:48 +0800

    1

--- electron-main/src/renderer/src/components/MainPage.vue
+++ electron-main/src/renderer/src/components/MainPage.vue
@@ -1,9 +1,22 @@
 <script setup>
+import {ref} from "vue";
+import TabCard from "./TabCard.vue";
 
+const tabIndex=ref(0)
 </script>
 
 <template>
-  <div class="relative left-0 top-0 w-full h-screen bg-zinc-200 dark:bg-zinc-800">
-    <div class="absolute size-12 left-0 right-0 top-0 bottom-0 m-auto bg-red-500"></div>
+  <div class="relative left-0 top-0 w-full h-screen bg-zinc-200 dark:bg-zinc-800 flex items-center justify-center">
+    <tab-card>
+      <template #item1>
+        item1
+      </template>
+      <template #item2>
+        item2
+      </template>
+      <template #item3>
+        item3
+      </template>
+    </tab-card>
   </div>
 </template>
---
+++ electron-main/src/renderer/src/components/TabCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from "vue";
+
+const tabIndex=ref(0)
+</script>
+
+<template>
+  <div class="relative left-0 top-0 flex flex-col items-center justify-center h-screen w-24  bg-zinc-300 dark:bg-zinc-900 p-4 gap-y-4">
+    <span @click="()=>tabIndex=0" :class="[tabIndex===0? 'text-cyan-400':'text-zinc-900 dark:text-zinc-200']">tab1</span>
+    <span @click="()=>tabIndex=1" :class="[tabIndex===1? 'text-cyan-400':'text-zinc-900 dark:text-zinc-200']">tab2</span>
+    <span @click="()=>tabIndex=2" :class="[tabIndex===2? 'text-cyan-400':'text-zinc-900 dark:text-zinc-200']">tab3</span>
+  </div>
+  <div class="bg-transparent w-full h-screen" v-show="tabIndex===0">
+    <slot name="item1"></slot>
+  </div>
+  <div class="bg-transparent w-full h-screen" v-show="tabIndex===1">
+    <slot name="item2"></slot>
+  </div>
+  <div class="bg-transparent w-full h-screen" v-show="tabIndex===2">
+    <slot name="item3"></slot>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
