837aaf7a9ea20da931e8f00e47b57f0de0bb4cf1
Empty <2167335204@qq.com>
2025-01-04 19:53:05 +0800

    1

---
+++ electron-main/src/renderer/src/components/MainPage.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="relative left-0 top-0 w-full h-screen bg-zinc-200 dark:bg-zinc-800">
+    <div class="absolute size-12 left-0 right-0 top-0 bottom-0 m-auto bg-red-500"></div>
+  </div>
+</template>
---
+++ electron-main/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important:true,
+  content: ["./src/renderer/**/*.{html,js,vue}"],
+  darkMode: 'selector',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: false,
+    darkTheme: "dark",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
+  },
+}
+
